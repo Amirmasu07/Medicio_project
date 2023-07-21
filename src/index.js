75baa09fd10ab714398b7dec90ae829f053a1774
Amir Masu <115700409+Amirmasu07@users.noreply.github.com>
2023-07-21 20:10:00 +0530

    Delete index.js

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import Mainrouter from './Mainrouter';
-import { RouterProvider } from 'react-router-dom';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<RouterProvider router={Mainrouter} />);
